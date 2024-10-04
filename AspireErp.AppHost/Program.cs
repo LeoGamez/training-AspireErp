74e082f295207748b3957903f96d40cffa883393
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2024-10-04 11:25:04 -0600

    initial creation and adding to aspire

--- AspireErp.AppHost/Program.cs
+++ AspireErp.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.HelloWorld_Api>("helloworld-api");
 
+builder.AddProject<Projects.AspireErp_Customers_Api>("aspireerp-customers-api");
+
 builder.Build().Run();
